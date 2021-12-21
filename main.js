b44ccb9ee32850fb109f81e27ac650838c54bdcc
Debikapaul <82105390+Debikapaul@users.noreply.github.com>
2021-12-21 16:53:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ Webcam.set({
 
     console.log('ml5 version:;',ml5.version);
 
-    classifier=ml5.imageClassifier('');
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/[...}');
 
 
     function check(){
